37356cd68672efa90d40013b51ea3a169c0f6bed
ericheep <ericheep@gmail.com>
2017-12-13 11:06:03 -0800

    :art: (math) moving math function to math dir

--- src/math/index.js
+++ src/math/index.js
@@ -1,2 +1,5 @@
 export const logarithmicScaleToFrequency = (power, arb = 20) =>
   (Math.pow(2, power) * arb).toFixed(2)
+
+export const frequencyToLogarithmicScale = (frequency, arb = 20) =>
+   Math.log2(frequency/arb)
